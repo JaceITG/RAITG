524ab92b8e8852a48b77a59fa0c1acf8e6ee8c60
Scheisse <jjwazoo@gmail.com>
2023-03-06 19:28:56 -0500

    include name in diff parsing

--- utils/parse_chart.py
+++ utils/parse_chart.py
@@ -1,5 +1,12 @@
 import os, sys
 
+# For a chart (start_ind, name, difficulty) in lines, write a file
+# containing metadata header followed by note data
+# converted to 192nd notes
+def format_diff(lines, chart):
+    pass
+
+# Read a file and extract information about bpm and difficulties
 def main(fp):
     if os.path.splitext(fp)[1] != '.ssc':
         return ValueError("Stepfile must be an .ssc")
@@ -12,7 +19,7 @@ def main(fp):
     bpmstr = ""
     readingbpm = False
 
-    # List of difficulties in file [(start_index, difficulty)]
+    # List of difficulties in file [(start_index, name, difficulty)]
     charts = []
     for i in range(len(lines)):
         if readingbpm:
@@ -34,20 +41,32 @@ def main(fp):
         if lines[i].startswith("#NOTEDATA"):
             startind = i
             
+            #Seek to difficulty name declaration
+            while not lines[i].startswith("#DIFFICULTY"):
+                i += 1
+            clean = lines[i].strip(";\n")
+            name = clean[clean.index(":")+1:]
+
             #Seek to difficulty meter declaration
             while not lines[i].startswith("#METER"):
                 i += 1
-            
             clean = lines[i].strip(";\n")
             difficulty = int(clean[clean.index(":")+1:])
-            charts.append((startind, difficulty))
+            charts.append((startind, name, difficulty))
             continue
+
     
     bpmstr = bpmstr.replace(',','\n')
     print(f"BPM breakdown:\n{bpmstr}")
     print()
     for c in charts:
-        print(f"Chart at line {c[0]}: Difficulty {c[1]}")
+        print(f"Chart at line {c[0]}: {c[1]} {c[2]}")
+
+    bpms = []
+    # Convert BPM string to floating point list of timing points (time, bpm)
+    for timingpoint in bpmstr.split(','):
+        values = (float(v) for v in timingpoint.split('='))
+        bpms.append(values)
             
 
 
