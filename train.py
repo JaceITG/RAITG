77e4597fd1fae1ad993f020086358372ef7e87c8
Scheisse <jjwazoo@gmail.com>
2023-03-18 14:46:09 -0400

    node progress bar

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ import autokeras as ak
 import pandas as pd
 import numpy as np
 import os
+from progress.bar import Bar
 
 # Create an array of AutoKeras StructuredDataInput nodes from the .cht files in dataset.
 # Each input node in shape [ beat[ arrows,bpm ], beat[ arrows,bpm ], ...]
@@ -19,41 +20,44 @@ def create_nodes(dataset):
             maxm = max(maxm, int(c))
             chart_count += 1
 
-    for f in os.listdir(dataset):
+    with Bar('Loading nodes...',suffix='%(percent).1f%% - %(eta)ds',max=len(os.listdir(dataset))) as bar:
+        for f in os.listdir(dataset):
 
-        #skip non-cht files
-        if not os.path.splitext(f)[1] == '.cht':
-            continue
+            #skip non-cht files
+            if not os.path.splitext(f)[1] == '.cht':
+                bar.next()
+                continue
 
-        fp = os.path.join(dataset,f)
-        row = []
-        difficulty = int( f[ f.rfind('[')+1 : f.rfind(']') ] )
+            fp = os.path.join(dataset,f)
+            row = []
+            difficulty = int( f[ f.rfind('[')+1 : f.rfind(']') ] )
 
-        notedata = []
-        with open(fp, 'r') as data:
-            datalines = data.readlines()
+            notedata = []
+            with open(fp, 'r') as data:
+                datalines = data.readlines()
 
-            for l in datalines:
-                #Turn info about a beat into array [arrows, bpm (float)]
-                pointinfo = l.strip('\n').split(',')
-                beat = [pointinfo[0], float(pointinfo[1])]
-                notedata.append(beat)
-            
-            #append empty measures if shorter than longest chart
-            difference = ((maxm+1)*192) - len(notedata)
-            if difference > 0:
-                lastbpm = notedata[-1][1]
-                blank = [["0000",lastbpm] for i in range(difference)]
-                notedata += blank
+                for l in datalines:
+                    #Turn info about a beat into array [arrows, bpm (float)]
+                    pointinfo = l.strip('\n').split(',')
+                    beat = [pointinfo[0], float(pointinfo[1])]
+                    notedata.append(beat)
+                
+                #append empty measures if shorter than longest chart
+                difference = ((maxm+1)*192) - len(notedata)
+                if difference > 0:
+                    lastbpm = notedata[-1][1]
+                    blank = [["0000",lastbpm] for i in range(difference)]
+                    notedata += blank
 
-        types = {'Note':'categorical', 'BPM':'Numerical'}
-        cname = os.path.basename(fp)
-        node = ak.StructuredDataInput(column_names=['Note', 'BPM'],column_types=types, name=cname)
-        inputs.append(node)
-        outputs.append(difficulty)
+            types = {'Note':'categorical', 'BPM':'Numerical'}
+            cname = os.path.basename(fp)
+            node = ak.StructuredDataInput(column_names=['Note', 'BPM'],column_types=types, name=cname)
+            inputs.append(node)
+            outputs.append(difficulty)
+            bar.next()
 
-    print(np.shape(inputs))
-    #print(arr[0,-10:])
+    print(f"{len(inputs)} nodes")
     
+    return inputs, outputs
 
 create_nodes('./data/dataset/')
