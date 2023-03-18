856da5c5720fc4b28c5001aeee2f804dd09790b8
Scheisse <jjwazoo@gmail.com>
2023-03-18 13:34:34 -0400

    normalize notedata lengths

--- train.py
+++ train.py
@@ -1,4 +1,4 @@
-import autokeras as ak
+#import autokeras as ak
 import pandas as pd
 import numpy as np
 import os
@@ -7,6 +7,14 @@ import os
 # Each chart in shape [difficulty, notedata[ beat[ arrows,bpm ], beat[ arrows,bpm ], beat[ arrows,bpm ], ...] ]
 def create_dframe(dataset):
     arr = []
+
+    maxm = 0
+    with open(os.path.join(dataset,'meascounts.txt')) as f:
+        #get max measure len from csv, ignoring trailing ,
+
+        for c in f.read()[:-1].split(','):
+            maxm = max(maxm, int(c))
+
 
     for f in os.listdir(dataset):
         fp = os.path.join(dataset,f)
@@ -22,6 +30,13 @@ def create_dframe(dataset):
                 pointinfo = l.strip('\n').split(',')
                 beat = [pointinfo[0], float(pointinfo[1])]
                 notedata.append(beat)
+            
+            #append empty measures if shorter than longest chart
+            difference = (maxm*192 + 1) - len(notedata)
+            if difference > 0:
+                lastbpm = notedata[-1][1]
+                blank = [["0000",lastbpm] for i in range(difference)]
+
         row = [difficulty, notedata]
         arr.append(np.array(row, dtype=[('a', np.int32), ('b', object)]))
 
@@ -30,4 +45,4 @@ def create_dframe(dataset):
     print(arr['a'])
     print(np.shape(arr))
 
-create_dframe('./data/testset/')
+create_dframe('./data/dataset/')
